f88e1d6f20c9379f67f10b8194b0d2179e8901a9
codebuildervaibhav <ranavaibhavprf@gmail.com>
2025-09-04 02:47:10 +0530

    initial auth routes

---
+++ routes/authors.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author');
+const e = require('express');
+
+// all authors route 
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i') // 'i' for case insensitive
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+// new author
+router.get('/new', (req, res) => {
+    res.render('authors/new' , {author: new Author()});
+})
+
+//create author route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('/authors')
+        // or: res.redirect(`/authors/${newAuthor.id}`)
+    } catch (err) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+})
+
+module.exports = router
